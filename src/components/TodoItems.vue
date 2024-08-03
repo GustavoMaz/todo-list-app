48bcf00c747f2be8faf18283cf1d29bb760e438c
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-08-03 16:02:22 -0300

    Adicionados os arquivos ao reposotório

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="space-y-2">
+        <TodoItem 
+            v-for="todo in reversedTodos"
+            :key="todo.id"
+            :todo="todo"
+        />
+    </div>
+</template>
+
+<script>
+import TodoItem  from '@/components/TodoItem';
+import { useStore }  from 'vuex';
+export default {
+    components: { TodoItem },
+
+    setup() {
+        const store = useStore();
+        const reversedTodos = store.state.todos.reverse();
+        return {
+            reversedTodos
+        }
+    }
+
+    /* Options api
+    data() {
+        return {
+            reversedTodos: this.$store.state.todos.reverse()
+        }
+    }
+    */
+}
+</script>
