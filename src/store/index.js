24e9f979b5edc72a3a6db846d7ab81e47a2bae64
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-08-04 13:55:18 -0300

    URL nova nas funções de Todo

--- src/store/index.js
+++ src/store/index.js
@@ -34,28 +34,28 @@ export default createStore({
     },
     actions: {
         getTodos({ commit }) {
-            return axios.get('http://localhost:3000/todos')
+            return axios.get('https://gustavomaz.github.io/todo-list-app/todo')
                 .then((response) => {
                     commit('storeTodos', response.data);
                 })
         },
 
         addTodo({ commit }, data) {
-            return axios.post('http://localhost:3000/todos', data)
+            return axios.post('https://gustavomaz.github.io/todo-list-app/todo', data)
                 .then((response) => {
                     commit('storeTodo', response.data);
                 });
         },
 
         updateTodo({ commit }, { id, data }) {
-            return axios.put(`http://localhost:3000/todos/${id}`, data)
+            return axios.put(`https://gustavomaz.github.io/todo-list-app/todo/${id}`, data)
                 .then((response) => {
                     commit('storeTodo', response.data);
                 });
         },
 
         deleteTodo({ commit },  id) {
-            return axios.delete(`http://localhost:3000/todos/${id}`)
+            return axios.delete(`https://gustavomaz.github.io/todo-list-app/todo/${id}`)
                 .then(() => {
                     commit('deleteTodo', id);
                 });
