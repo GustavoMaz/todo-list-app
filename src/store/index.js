2cb10eac2c7d1ce12c1315a50fc98d23a886ba5f
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-08-06 18:34:21 -0300

    Corrigindo as URLs do axios

--- src/store/index.js
+++ src/store/index.js
@@ -34,35 +34,35 @@ export default createStore({
     },
     actions: {
         getTodos({ commit }) {
-          return axios.get('https://gustavomaz.github.io/todo-list-app/database.json/todo')  
-          //return axios.get('http://localhost:8080/todo-list-app')
-                .then((response) => {
-                    commit('storeTodos', response.data);
-                })
+          //return axios.get('https://gustavomaz.github.io/todo-list-app/dist/database.json/todos')  
+          return axios.get('http://localhost:3000/todos')
+            .then((response) => {
+                commit('storeTodos', response.data);
+            })
         },
 
         addTodo({ commit }, data) {
-          return axios.post('https://gustavomaz.github.io/todo-list-app/database.json/todo', data)  
-          //return axios.post('http://localhost:8080/todo-list-app/todo', data)
-                .then((response) => {
-                    commit('storeTodo', response.data);
-                });
+          //return axios.post('https://gustavomaz.github.io/todo-list-app/database.json/todos', data)  
+          return axios.post('http://localhost:3000/todos', data)
+            .then((response) => {
+                commit('storeTodo', response.data);
+            });
         },
 
         updateTodo({ commit }, { id, data }) {
-          return axios.put(`https://gustavomaz.github.io/todo-list-app/database.json/todo/${id}`, data)  
-          //return axios.put(`http://localhost:8080/todo-list-app/todo/${id}`, data)
-                .then((response) => {
-                    commit('storeTodo', response.data);
-                });
+          //return axios.put(`https://gustavomaz.github.io/todo-list-app/database.json/todos/${id}`, data)  
+          return axios.put(`http://localhost:3000/todos/${id}`, data)
+            .then((response) => {
+                commit('storeTodo', response.data);
+            });
         },
 
         deleteTodo({ commit },  id) {
-          return axios.delete(`https://gustavomaz.github.io/todo-list-app/database.json/todo/${id}`)  
-          //return axios.delete(`http://localhost:8080/todo-list-app/todo/${id}`)
-                .then(() => {
-                    commit('deleteTodo', id);
-                });
+          //return axios.delete(`https://gustavomaz.github.io/todo-list-app/database.json/todos/${id}`)  
+          return axios.delete(`http://localhost:3000/todos/${id}`)
+            .then(() => {
+                commit('deleteTodo', id);
+            });
         }
     },
     modules: {
